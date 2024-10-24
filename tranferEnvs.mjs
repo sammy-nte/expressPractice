c15e6266e9a24f31d936590eeb909a9ff01777b2
Kwabena <sammyntewusu1@gmail.com>
2024-10-24 17:55:45 +0000

    aaaaaaa

--- tranferEnvs.mjs
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const transferEnvsRouter = Router();
-
-transferEnvsRouter.get("/", (req, res) => {
-  res.send("transfer endpoint");
-});
-
-export default transferEnvsRouter;
